e81e899d122e7553301d5f90c836c636ec2c0d63
Hujba <163361586+Hujba@users.noreply.github.com>
2026-07-30 05:40:58 +0000

    Fixed temperature

--- starter.py
+++ starter.py
@@ -1,5 +1,13 @@
 # The raw passphrase to clean:
 raw_phrase = "aP!pL3e#S4aU%cE"
+symbols_to_remove = "!#%"
+clean_phrase = " "
 
+for char in raw_phrase:
+    if char not in symbols_to_remove and not char .isdigit():
+        clean_phrase += char
+
+final_code = clean_phrase.upper()
+print(f"Decoded Code: {final_code}")        
 # YOUR GOAL: Clean up this phrase using the 3 Security Rules below!
 
